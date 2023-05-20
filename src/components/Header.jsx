4d3d8f86c99e35e2c856c965d21e4553085ea862
shashank <sarthanur@gmail.com>
2023-05-20 15:33:54 +0530

    Header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Components.css"
 
 const Header = () => {
     return (
@@ -7,29 +8,29 @@ const Header = () => {
                 <img className="logo" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478805/Google%20docs/61447cd55953a50004ee16d9_wswicx.png" alt="" />
                 <div className="header-info">
                     <span>
-                        <h1>Untitled document</h1>
-                        <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478679/Google%20docs/grade_FILL0_wght400_GRAD0_opsz48_vjrilj.png" alt="" />
+                        <span className="title" >Untitled document</span>
+                        <img className="star"  src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478679/Google%20docs/grade_FILL0_wght400_GRAD0_opsz48_vjrilj.png" alt="" />
                     </span>
-                    <ul>
-                        <li>File </li>
-                        <li> Edit </li>
-                        <li> View </li>
-                        <li>Insert </li>
-                        <li> Format </li>
-                        <li> Tools</li>
-                        <li> Extensions </li>
-                        <li> Help </li>
-                    </ul>
+                    <div>
+                      <span>File </span>
+                      <span>Edit </span>
+                      <span>View </span>
+                      <span>Insert </span>
+                      <span>Format </span>
+                      <span>Tools </span>
+                      <span>Extensions </span>
+                      <span>Help </span>
+                    </div>
                 </div>
                 <div className="header-right">
-                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478677/Google%20docs/comment_FILL0_wght400_GRAD0_opsz48_rldp5h.png" alt="" />
+                    <img className="header-icon" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478677/Google%20docs/comment_FILL0_wght400_GRAD0_opsz48_rldp5h.png" alt="" />
                     <div>
-                        <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478677/Google%20docs/videocam_FILL0_wght400_GRAD0_opsz48_tfbmoa.png" alt="" />
-                        <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" />
+                        <img className="header-icon" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478677/Google%20docs/videocam_FILL0_wght400_GRAD0_opsz48_tfbmoa.png" alt="" />
+                        <img className="header-icon" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" />
                     </div>
                     <div>
-                        <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478677/Google%20docs/lock_FILL0_wght400_GRAD0_opsz48_smdahw.png" alt="" />
-                        <p>Share</p>
+                        <img className="lock" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478677/Google%20docs/lock_FILL0_wght400_GRAD0_opsz48_smdahw.png" alt="" />
+                        {/* <span>Share</span> */}
                     </div>
                     <img className="profile-pic" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1683897267/cosmetics/pexels-jonaorle-3828268_ctpqw3.jpg" alt="" />
                 </div>
