066362f879c6845c0370dad29c216d8a4b9f3460
shashank <sarthanur@gmail.com>
2023-05-21 19:06:19 +0530

    tool3 done

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -15,7 +15,8 @@ const Tools = () => {
                         <img className="icon" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" />
                     </div>
                 </div>
-                <div className="line" />  
+                <div className="line" /> 
+                {/* ///////////////////////////////////////////////  */}
                 <div className="tool2">
                     <div className="extra">
                         <span>Normal text</span>
@@ -26,6 +27,13 @@ const Tools = () => {
                         <span>Arial</span>
                         <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" className="icon" />
                     </div>
+                </div>
+                <div className="line"></div>
+                {/* //////////////////////////////////////////////////// */}
+                <div className="tool3">
+                    <span className="extra">-</span>
+                    <span className="number">11</span>
+                    <span className="extra">+</span>
                 </div>
                 <div className="line"></div>
             </div>
