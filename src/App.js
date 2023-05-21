e12797ad0f2e15860491511c9cd4523e87e51bf0
shashank <sarthanur@gmail.com>
2023-05-21 18:31:24 +0530

    Tool1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
 import Header from "./components/Header";
+import Tools from "./components/Tools";
 
 
 function App() {
   return (
     <>
       <Header />
+      <Tools />
     </>
   );
 }
---
+++ src/components/Tools.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Tools = () => {
+    return (
+        <>
+            <div className="tool-section">
+                <div className="tool1">
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478667/Google%20docs/undo_FILL0_wght400_GRAD0_opsz48_rbqbnq.png" alt="" className="icon" />
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478667/Google%20docs/redo_FILL0_wght400_GRAD0_opsz48_zzzalv.png" alt="" className="icon" />
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478667/Google%20docs/print_FILL0_wght400_GRAD0_opsz48_ahtavo.png" alt="" className="icon" />
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478668/Google%20docs/spellcheck_FILL0_wght400_GRAD0_opsz48_t1a8hz.png" alt="" className="icon" />
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478668/Google%20docs/imagesearch_roller_FILL0_wght400_GRAD0_opsz48_awthrf.png" alt="" className="icon" />
+                    <div className="extra">
+                        <span>100 %</span>
+                        <img className="icon" src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" />
+                    </div>
+                </div>
+                <div className="line" />  
+            </div>
+        </>
+    );
+};
+
+export default Tools;
