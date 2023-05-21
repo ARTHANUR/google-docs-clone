030379744a122d25dc7acf3546d26c2f36f70f32
shashank <sarthanur@gmail.com>
2023-05-21 19:56:48 +0530

    tool5 done

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -45,6 +45,36 @@ const Tools = () => {
                     <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478669/Google%20docs/format_ink_highlighter_FILL0_wght400_GRAD200_opsz40_k0kbvx.png" alt="" className="icon" />
                 </div>
                 <div className="line"></div>
+                {/* ///////////////////////////////////////////////////////// */}
+                <div className="tool5">
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677849/Google%20docs/link_FILL0_wght400_GRAD0_opsz48_rmjbfx.png " alt="" className="icon" />
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677830/Google%20docs/add_comment_FILL0_wght400_GRAD0_opsz48_xkk5ep.png " alt="" className="icon" />
+                    <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677829/Google%20docs/panorama_FILL0_wght400_GRAD0_opsz48_cw7vnb.png" alt="" className="icon" />
+                </div>
+                <div className="line"></div>
+                {/* ///////////////////////////////////////////////////////////// */}
+                {/* <div className="tool6">
+                    <div className="extra">
+                        <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677829/Google%20docs/format_align_left_FILL0_wght400_GRAD0_opsz48_bnknjw.png" alt="" className="icon" />
+                        <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" className="icon" />
+                    </div>
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677829/Google%20docs/format_line_spacing_FILL0_wght400_GRAD0_opsz48_a7n8mg.png" alt="" className="icon" />
+                    <div className="extra">
+                        <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677830/Google%20docs/checklist_FILL0_wght400_GRAD0_opsz48_ksir4a.png" alt="" className="icon" />
+                        <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" className="icon" />
+                    </div>
+                    <div className="extra">
+                        <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677830/Google%20docs/format_list_bulleted_FILL0_wght400_GRAD0_opsz48_xjonag.png" alt="" className="icon" />
+                        <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png" alt="" className="icon" />
+                    </div>
+                    <div className="extra">
+                        <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677830/Google%20docs/format_list_numbered_FILL0_wght400_GRAD0_opsz48_tofbm6.png " alt="" className="icon" />
+                        <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684478675/Google%20docs/arrow_drop_down_FILL0_wght400_GRAD0_opsz48_rplvdb.png " alt="" className="icon" />
+                    </div>
+                    <img src="https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677830/Google%20docs/format_indent_increase_FILL0_wght400_GRAD0_opsz48_uhokuk.png " alt="" className="icon"/>
+                    <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677830/Google%20docs/format_indent_increase_FILL0_wght400_GRAD0_opsz48_uhokuk.png" alt="" className="icon"/>
+                    <img src=" https://res.cloudinary.com/dboa7dqkl/image/upload/v1684677829/Google%20docs/format_clear_FILL0_wght400_GRAD0_opsz48_gfyb4i.png" alt="" className="icon"/>
+                </div> */}
             </div>
         </>
     );
